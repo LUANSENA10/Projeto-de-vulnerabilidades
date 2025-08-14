93874683bfcf592c8ee8a1e9458eba94d5bf8e7a
luanpaivadev <luanpaivadev@gmail.com>
2025-08-14 13:07:12 -0300

    Update UserEntity.java

--- src/main/java/com/security/security/repository/model/UserEntity.java
+++ src/main/java/com/security/security/repository/model/UserEntity.java
@@ -5,8 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
